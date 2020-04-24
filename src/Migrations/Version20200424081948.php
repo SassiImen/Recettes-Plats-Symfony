4002fdecd5e53be48ca882599423e607f39232b9
sassi imen <sassiimen72@gmail.com>
2020-04-24 10:21:28 +0200

    first commit

--- src/Migrations/Version20200424081948.php
+++ src/Migrations/Version20200424081948.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200407074232 extends AbstractMigration
+final class Version20200424081948 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,8 @@ final class Version20200407074232 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE plat ADD category_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE plat ADD CONSTRAINT FK_2038A20712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
-        $this->addSql('CREATE INDEX IDX_2038A20712469DE2 ON plat (category_id)');
+        $this->addSql('ALTER TABLE plat CHANGE score_id score_id INT DEFAULT NULL, CHANGE category_id category_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -33,9 +31,7 @@ final class Version20200407074232 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE plat DROP FOREIGN KEY FK_2038A20712469DE2');
-        $this->addSql('DROP TABLE category');
-        $this->addSql('DROP INDEX IDX_2038A20712469DE2 ON plat');
-        $this->addSql('ALTER TABLE plat DROP category_id');
+        $this->addSql('ALTER TABLE plat CHANGE score_id score_id INT DEFAULT NULL, CHANGE category_id category_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
     }
 }
